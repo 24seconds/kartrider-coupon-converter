deb81d57eec7d4fe65f5b386980b3a649357e342
GeunYeong Cheon <24crazyoung@gmail.com>
2019-09-01 12:54:09 +0900

    Updates

---
+++ precache-manifest.d2a76a772759ffabc98e8805047e86f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c9a52f2aaf5ce2fb404e1672ce29d4",
+    "url": "/kartrider-coupon-converter/index.html"
+  },
+  {
+    "revision": "39c46462f3670d1f60b1",
+    "url": "/kartrider-coupon-converter/static/css/main.90f84ebf.chunk.css"
+  },
+  {
+    "revision": "9da978702f8b4a5c1a38",
+    "url": "/kartrider-coupon-converter/static/js/2.4ffe02b4.chunk.js"
+  },
+  {
+    "revision": "39c46462f3670d1f60b1",
+    "url": "/kartrider-coupon-converter/static/js/main.2eb8d9f1.chunk.js"
+  },
+  {
+    "revision": "efdc7c6ec251fa46c9b5",
+    "url": "/kartrider-coupon-converter/static/js/runtime~main.690deaf8.js"
+  }
+]);
